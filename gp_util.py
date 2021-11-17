b7a612bce4c549c3c76833599a97c8f5b7710dd2
IvanHCenalmor <ivanhcenalmor@gmail.com>
2021-11-17 11:56:07 +0100

    Numpy in the objective function

--- gp_util.py
+++ gp_util.py
@@ -25,16 +25,18 @@ def parser(filepath):
     with open(filepath, "r") as f:
         n = int(f.readline())
         m = [[int(i) for i in line.split('   ')[:-1]] for line in f.readlines()]
-        return np.array(m,dtype="uint16")
+        return n, np.array(m,dtype="uint16")
 
 def objective_function(graph, solution):
-    res = 0
     
-    for i in range(0,len(graph)-1):
-        for j in range(i+1,len(graph)):
-            res += abs(solution[i]-solution[j])*graph[i,j]
-            
-    return res
+    aux_sol = np.array([solution])
+    
+    A = (1 - aux_sol) * aux_sol.transpose()
+
+    W = A + A.transpose()
+    
+    return np.sum(graph*W)/2    
+    
     
 def random_solution(n):
     return np.array(random.sample([True]*(n//2)+[False]*(n//2),n),dtype="bool8")     
